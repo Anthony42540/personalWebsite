0e983be19d2ae55f4d74729129d8808e90f1855d
Anthony42540 <anthonydavila90@gmail.com>
2025-09-30 18:19:34 -0400

    Vercel setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import svelte from '@astrojs/svelte';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  adapter: vercel({}),
+  output: 'server',
 
   integrations: [svelte()]
 });
